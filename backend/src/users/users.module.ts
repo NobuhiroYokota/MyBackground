39fbef1ef1d7e02c1b96bcf02b49ee99106319d4
nobuhiroyokota <yokota.nobuhiro.job@gmail.com>
2024-11-10 03:17:02 +0900

    prismaを用いたSupabaseにuser追加機能の実装

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { UserService } from './users.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UserService, PrismaService],
 })
 export class UsersModule {}
